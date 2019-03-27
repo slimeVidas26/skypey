e96badab6ebd9e7e17221a9bf1e53ee51c5cecfc
SlimeVidas <dahan.isaac@gmail.com>
2019-03-27 20:03:01 +0200

    create Sidebar.js and Sidebar.css

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = ()=> {
+  return (
+    <aside className = "Sidebar">
+      sidebar
+    </aside>
+  )
+}
+
+export default Sidebar
